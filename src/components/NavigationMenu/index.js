ceb96b1be9b96eea0a28e90d5c78b3b559da36a3
Jakov Spahija <jspahi00@fesb.hr>
2020-12-09 20:46:41 +0100

    Dodan drop down tip tabova

--- src/components/NavigationMenu/index.js
+++ src/components/NavigationMenu/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
- 
 import styles from './style.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { navTabs } from '../../constants/constant.js'
+import { dropDownTabs } from '../../constants/constant.js'
 
-const navTabs = ['Naslovna', 'Tečajevi', 'Oprema', 'Izleti', 'Arhiva', 'Blog']
 
 const NavigationMenu = ({activeTab}) => (
     <nav className={styles.navigationMenu}>
       <ul>
       {navTabs.map(tab => <li className={tab === activeTab ? styles.active : ''}>
-        {tab}</li>
+        {tab}
+        { dropDownTabs.includes(tab) ? 
+          <FontAwesomeIcon icon={faChevronDown} style={  {paddingLeft: "5px"}} /> : 
+          ''}
+        </li>
       )}
       </ul>
     </nav>
---
+++ src/constants/constant.js
@@ -0,0 +1,3 @@
+export const navTabs = ['Naslovna', 'Tečajevi', 'Oprema', 'Izleti', 'Arhiva', 'Blog']
+export const dropDownTabs = ['Tečajevi','Izleti']
+
