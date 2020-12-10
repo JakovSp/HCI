cb80049b6844fc22ece46c3fb071686a7c577e35
ivevasiljevic <ive@profico.hr>
2020-12-10 16:54:33 +0100

    add blog and news footer component

---
+++ src/components/blog-and-news-footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const BlogNewsFooter = () => (
+    <main>
+        <h2 className={styles.header}>Blog i novosti</h2>
+        <ul className={styles.contact_list}>
+            <li>Planinarenje s djecom</li>
+            <li>Savjeti HGSS-a za sigurno planinarenje</li>
+            <li>Najbolja oprema za slobodno penjanje</li>
+        </ul>
+    </main>
+)
+
+export default BlogNewsFooter
