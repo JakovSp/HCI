4a9e5a00e7065942e83d2aaa658165de45b4c82c
ivevasiljevic <ive@profico.hr>
2020-12-10 16:55:56 +0100

    add copyright footer component

---
+++ src/components/copyright-footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const CopyrightFooter = () => (
+    <main className={styles.container}>
+        <span>Rockreacija</span>
+        <span>© 2021 Rockreacija. Napravljeno u sklopu kolegija Korisnička Sučelja.</span>
+    </main>
+)
+
+export default CopyrightFooter
