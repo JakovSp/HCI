c39a53bb420d36a5617e98c90fe5f94122a6976f
Jakov Spahija <jspahi00@fesb.hr>
2020-12-16 17:26:51 +0100

    Update header-footer layout

--- src/layout/header-footer.js
+++ src/layout/header-footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ContactBar from '../components/contact-bar'
 import Footer from '../modules/footer'
+import NavigationHeader from '../components/NavigationHeader'
 
 const HeaderFooterLayout = ({children}) => (
     <>
         <ContactBar/>
+        <NavigationHeader />
             {children}
         <Footer/>
     </>
