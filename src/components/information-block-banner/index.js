767dc8e5124fe8069ef74decbdde25f83446b877
ivevasiljevic <ive@profico.hr>
2020-12-17 22:08:47 +0100

    remove bootstrap

--- src/components/information-block-banner/index.js
+++ src/components/information-block-banner/index.js
@@ -2,15 +2,14 @@ import React from 'react'
 import styles from './style.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import Button from 'react-bootstrap/Button'
 
 const BannerInformationBlock = () => (
     <main className={styles.container}>
         <h2>Postanite član Planinarskog kluba Rockreacija već danas i uživajte u brojnim pogodnostima.</h2>
-        <Button variant="primary" className={styles.button_custom}>
+        <a className={styles.button_custom}>
             <span>Učlani se</span>
             <FontAwesomeIcon className={styles.arrow} icon={faChevronRight} color="white" />
-        </Button>
+        </a>
     </main>
 )
 
