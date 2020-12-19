ec3a61426b39954e062b1b63d6da19b3adf51d62
ivevasiljevic <ive@profico.hr>
2020-12-19 20:04:30 +0100

    add courses section

---
+++ src/components/courses-information/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './style.module.css'
+import Button from '../button'
+
+const CoursesInformation = () => (
+    <main className={styles.container}>
+        <h1 className={styles.courses_header}>Početnik ste i mislite da je pravo vrijeme da naučite kako navigirati planinama?<br></br>Prijavi se na naše tečajeve.</h1>
+        <Button style={styles.courses_button} text="Prijavi se"/>
+    </main>
+)
+
+export default CoursesInformation
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import HeaderFooterLayout from "../layout/header-footer"
 import HomePageBanner from "../modules/home-page-banner"
+import HomePageCourses from "../modules/home-page-courses"
 import HomePageNews from "../modules/home-page-news"
 
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Naslovna">
     <HomePageBanner/>
     <HomePageNews/>
+    <HomePageCourses/>
   </HeaderFooterLayout>
 )
 
