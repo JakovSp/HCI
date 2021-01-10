5aa38b8a5d1e049ab69dfe68f909be7c1cdac9aa
ivevasiljevic <ive@profico.hr>
2021-01-10 22:33:34 +0100

    minor improvements

--- src/components/card-month/index.js
+++ src/components/card-month/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import styles from './style.module.css'
 import CardMonthDay from '../card-month-day'
+import styled from 'styled-components';
 
-const CardMonth = ({monthName, monthColor}) => (
-    <main className={styles.month_container}>
+const CardMonth = ({monthName, monthColor, onClickEvent}) => {
+
+    const HoverContainer = styled.main`
+	:hover {
+		color: ${props => props.inputColor};
+		cursor: pointer;
+	}
+`
+
+    return (
+    <HoverContainer inputColor={monthColor} className={styles.month_container} onClick={ onClickEvent }>
         <p className={styles.month_name}>{monthName}</p>
         <div className={styles.month_hikings_container}>
             <CardMonthDay monthColor={monthColor}/>
@@ -12,8 +22,15 @@ const CardMonth = ({monthName, monthColor}) => (
             <CardMonthDay monthColor={monthColor}/>
             <CardMonthDay monthColor={monthColor}/>
             <CardMonthDay monthColor={monthColor}/>
+            <CardMonthDay monthColor={monthColor}/>
+            <CardMonthDay monthColor={monthColor}/>
+            <CardMonthDay monthColor={monthColor}/>
+            <CardMonthDay monthColor={monthColor}/>
+            <CardMonthDay monthColor={monthColor}/>
+            <CardMonthDay monthColor={monthColor}/>
         </div>
-    </main>
-)
+    </HoverContainer>
+    )
+}
 
 export default CardMonth
--- src/components/hikings/index.js
+++ src/components/hikings/index.js
@@ -7,6 +7,7 @@ import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { useStaticQuery, graphql } from "gatsby"
 import { months } from '../../constants/constant'
 import CardMonth from '../card-month'
+import Ripple from '../ripple/ripple'
 
 export function getCurrentMonth(){
 
@@ -28,6 +29,8 @@ const Hikings = ({}) => {
         setNewView(newView);
     };
 
+    
+
     const data = useStaticQuery(graphql`
         query {
             myImage: file(relativePath: { eq: "planina.jpg" }) {
@@ -43,20 +46,33 @@ const Hikings = ({}) => {
     return (
         <main className={styles.container}>
         <div className={styles.control_container}>
-            <a style={{ visibility: (currentView == "month") ? 'visible' : 'hidden'}} onClick={ () => toggleMonth(getCurrentMonth()) } className={(currentMonth.monthNumber == getCurrentMonth()) ? styles.current_month_button_disable : styles.current_month_button_enable}>Trenutni mjesec</a>
+            <a style={{ visibility: (currentView == "month") ? 'visible' : 'hidden'}} onClick={ () => toggleMonth(getCurrentMonth()) } className={(currentMonth.monthNumber == getCurrentMonth()) ? styles.current_month_button_disable : styles.current_month_button_enable}>
+                Trenutni mjesec
+                <Ripple />
+            </a>
             <div style={{ visibility: (currentView == "month") ? 'visible' : 'hidden'}} className={styles.inner_control_container}>
                 <a onClick={ () => toggleMonth(currentMonth.monthNumber-1) } className={(currentMonth.monthNumber == 1) ? styles.arrow_disable : styles.arrow_enable}><FontAwesomeIcon icon={faArrowLeft} color="black" /></a>
                 <a className={ styles.month_title }>{ currentMonth.monthName } 2021</a>
                 <a onClick={ () => toggleMonth(currentMonth.monthNumber+1) } className={(currentMonth.monthNumber == 12) ? styles.arrow_disable : styles.arrow_enable}><FontAwesomeIcon icon={faArrowRight} color="black" /></a>
             </div>
             <ButtonGroup>
-                <a className={(currentView == "month") ? styles.button_group_active : styles.button_group_unactive} onClick={ () => toggleView("month") }>Mjesec</a>
-                <a className={(currentView == "year") ? styles.button_group_active : styles.button_group_unactive} onClick={ () => toggleView("year") }>Godina</a>
+                <a className={(currentView == "month") ? styles.button_group_active : styles.button_group_unactive} onClick={ () => toggleView("month") }>
+                    Mjesec
+                    <Ripple />
+                </a>
+                <a className={(currentView == "year") ? styles.button_group_active : styles.button_group_unactive} onClick={ () => toggleView("year") }>
+                    Godina
+                    <Ripple />
+                </a>
             </ButtonGroup>
         </div>
         <div>
-            <div className={styles.hikings_container_month} style={{ display: (currentView == "month") ? 'flex' : 'none'}}>
+            <div className={styles.hikings_container_month} style={{ display: (currentView == "month") ? 'grid' : 'none'}}>
                 <CardHiking hikingImage={data.myImage.childImageSharp.fluid} hikingTitle="News title" hikingDesc="Some quick example text to build on the card title and make up the bulk of the card's content." hikingFooter="bla"/
+                ><CardHiking hikingImage={data.myImage.childImageSharp.fluid} hikingTitle="News title" hikingDesc="Some quick example text to build on the card title and make up the bulk of the card's content." hikingFooter="bla"/
+                ><CardHiking hikingImage={data.myImage.childImageSharp.fluid} hikingTitle="News title" hikingDesc="Some quick example text to build on the card title and make up the bulk of the card's content." hikingFooter="bla"/
+                ><CardHiking hikingImage={data.myImage.childImageSharp.fluid} hikingTitle="News title" hikingDesc="Some quick example text to build on the card title and make up the bulk of the card's content." hikingFooter="bla"/
+                ><CardHiking hikingImage={data.myImage.childImageSharp.fluid} hikingTitle="News title" hikingDesc="Some quick example text to build on the card title and make up the bulk of the card's content." hikingFooter="bla"/
                 ><CardHiking hikingImage={data.myImage.childImageSharp.fluid} hikingTitle="News title" hikingDesc="Some quick example text to build on the card title and make up the bulk of the card's content." hikingFooter="bla"/
                 ><CardHiking hikingImage={data.myImage.childImageSharp.fluid} hikingTitle="News title" hikingDesc="Some quick example text to build on the card title and make up the bulk of the card's content." hikingFooter="bla"/
                 ><CardHiking hikingImage={data.myImage.childImageSharp.fluid} hikingTitle="News title" hikingDesc="Some quick example text to build on the card title and make up the bulk of the card's content." hikingFooter="bla"/
@@ -64,9 +80,9 @@ const Hikings = ({}) => {
                 >
 
             </div>
-            <div className={styles.hikings_container_year} style={{ display: (currentView == "year") ? 'flex' : 'none'}}>
+            <div className={styles.hikings_container_year} style={{ display: (currentView == "year") ? 'grid' : 'none'}}>
                 {months.map(({monthNumber, monthName, monthColor}) => (
-                    <CardMonth monthName={monthName} monthColor={monthColor}/>)
+                    <CardMonth onClickEvent={ () => { toggleView("month"); toggleMonth(monthNumber) }}  monthName={monthName} monthColor={monthColor}/>)
                 )}
             </div>
         </div>
