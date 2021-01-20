8017b22d83ff5d720c5a101412f7430a1a3b550b
ivevasiljevic <ive@profico.hr>
2021-01-20 11:49:06 +0100

    make main site logo clickable and navigate home

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from './style.module.css'
 import Logoimg from './../../images/new-logo.svg'
+import { navigate } from 'gatsby'
 
 const Logo = () => (
-    <img className={styles.logo} src={Logoimg} alt=''></img>
+    <img onClick={ () => navigate("/") } className={styles.logo} src={Logoimg} alt=''></img>
 )
 
 export default Logo
