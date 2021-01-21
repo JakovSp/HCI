607839e59fda91e5cad1272a7cf100766a13b8f1
ivevasiljevic <ive@profico.hr>
2021-01-21 10:24:26 +0100

    add working links in footer for menu

--- src/components/menu-footer/index.js
+++ src/components/menu-footer/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import styles from './style.module.css'
+import { Link } from 'gatsby'
+import { navTabs } from '../../constants/constant'
 
 const MenuFooter = () => (
     <main className={styles.container}>
         <h2 className={styles.header}>Menu</h2>
-        <ul className={styles.menu_list}>
-            <li>Naslovnica</li>
-            <li>Izleti</li>
-            <li>Oprema</li>
-            <li>Tečajevi</li>
-            <li>Arhiva</li>
-            <li>Blog</li>
-        </ul>
+        <nav className={styles.menu_list}>
+            {navTabs.map(({tab, to}) => (
+                <Link to={to} >
+                    <li><span>{tab}</span></li>
+                </Link>)
+            )}
+        </nav>
     </main>
 )
 
